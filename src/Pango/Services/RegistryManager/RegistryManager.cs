0c6b7aa2c5e48bae9cb10546dd3f11d7446efc20
kallebysantos <kalleby_santos@hotmail.com>
2025-03-18 23:42:08 +0000

    feat: pack components with brotli compression

--- src/Pango/Services/RegistryManager/RegistryManager.cs
+++ src/Pango/Services/RegistryManager/RegistryManager.cs
@@ -1,3 +1,4 @@
+using System.IO.Compression;
 using Pango.Abstractions;
 using Pango.Extensions;
 using Pango.Types;
@@ -62,4 +63,37 @@ public class RegistryManager() : IRegistryManager
 
     protected static string ResolveComponentSource(FileInfo componentFile) =>
         componentFile.Name.Replace(componentFile.Extension, string.Empty);
+
+    public async Task<Result<ComponentMetadata, IRegistryError>> PackComponent(
+        PackComponentInput packInput
+    )
+    {
+        // TODO: Maybe use AsyncEnumerable to yield compressed files
+        var toPackTasks = packInput.Metadata.Files.Select(async fileName =>
+        {
+            // TODO: Consider using StringBuilder to improve performance
+            var originalFileExtension = Path.GetExtension(fileName);
+            var outputFileName = Path.ChangeExtension(fileName, $"{originalFileExtension}.br");
+            var ouputDir = Path.Combine(packInput.OutputPath, packInput.Metadata.Source);
+
+            // Ensures ouput dir exists
+            Directory.CreateDirectory(ouputDir);
+
+            using var inputFileStream = File.OpenRead(
+                Path.Combine(packInput.InputBasePath, fileName)
+            );
+            using var outputFileStream = File.Create(
+                Path.Combine(packInput.OutputPath, outputFileName)
+            );
+            using var compressor = new BrotliStream(outputFileStream, CompressionLevel.Optimal);
+            await inputFileStream.CopyToAsync(compressor);
+
+            return outputFileName;
+        });
+
+        return packInput.Metadata with
+        {
+            Files = await Task.WhenAll(toPackTasks),
+        };
+    }
 }

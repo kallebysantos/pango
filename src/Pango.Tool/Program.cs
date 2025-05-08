440f5e9f94dae83e833c88a3462a2f4f4d51e3c5
kallebysantos <kalleby_santos@hotmail.com>
2025-05-08 11:23:39 +0100

    chore: upgrade deps
    - Spectre.Console: 0.48 -> 0.50

--- src/Pango.Tool/Program.cs
+++ src/Pango.Tool/Program.cs
@@ -1,8 +1,7 @@
-﻿using Spectre.Console;
-using Spectre.Console.Cli;
-
+﻿using System.Reflection;
 using Pango.Commands;
-using System.Reflection;
+using Spectre.Console;
+using Spectre.Console.Cli;
 
 var app = new CommandApp();
 var version = Assembly.GetExecutingAssembly().GetName()?.Version ?? new();
@@ -15,34 +14,55 @@ app.Configure(config =>
     // Display Pango logo
     if (args.Length == 0)
     {
-        AnsiConsole.MarkupLine(@"
+        AnsiConsole.MarkupLine(
+            @"
 [gold1]                                         [/][orangered1]      _/\/\/\/\/\/\-_     [/]
 [gold1]  ____                                   [/][orangered1]   _-//\/\/\/\/\/\/\/\>   [/]
 [gold1] |  _ \    __ _   _ __     __ _    ___   [/][orangered1]  /  \/\/\/\/\/\/\/\/\\>  [/]
 [gold1] | |_) |  / _` | | '_ \   / _` |  / _ \  [/][orangered1] / .  \/\/\/\/\/\/\/\/\\> [/]
 [gold1] |  __/  | (_| | | | | | | (_| | | (_) | [/][orangered1] |    __--\__--/\/\/\/\/> [/]
 [gold1] |_|      \__,_| |_| |_|  \__, |  \___/  [/][orangered1] \__//  \| |\_____/   //  [/]
-[gold1]                          |___/          [/][orangered1]   /    (_(_\________//   [/]");
+[gold1]                          |___/          [/][orangered1]   /    (_(_\________//   [/]"
+        );
         AnsiConsole.MarkupInterpolated($"\n[grey]Pango v{version.ToString(3)}[/] - ");
-        AnsiConsole.MarkupInterpolated($"[bold]Capture ants... I mean, components to your project[/]\n\n");
+        AnsiConsole.MarkupInterpolated(
+            $"[bold]Capture ants... I mean, components to your project[/]\n\n"
+        );
     }
 
-    config.AddCommand<DownloadComponent>("add")
+    config
+        .AddCommand<DownloadComponent>("add")
         .WithDescription("Downloads a component from the specified registry.")
         .WithExample("add", "button");
 
-    config.AddCommand<ConfigurationInit>("init")
+    config
+        .AddCommand<ConfigurationInit>("init")
         .WithDescription("Creates a Pango configuration file.")
-        .WithExample("init", "-n MyProject.Components", "-o src/MyProject.Components", "--registry-uri https://raw.github.com/repo");
+        .WithExample(
+            "init",
+            "-n MyProject.Components",
+            "-o src/MyProject.Components",
+            "--registry-uri https://raw.github.com/repo"
+        );
 
-    config.AddBranch("registry", cmd =>
-    {
-        cmd.SetDescription("Tools for managing registry");
-        cmd.AddCommand<ComponentRegisterCreation>("create-metadata")
-            .WithDescription("Creates registry metadata for the given components.")
-            .WithExample("registry", "create-metadata", "Components/*", "-o", "dist/components", "--registry-uri", "https://raw.github.com/repo");
-    });
+    config.AddBranch(
+        "registry",
+        cmd =>
+        {
+            cmd.SetDescription("Tools for managing registry");
+            cmd.AddCommand<ComponentRegisterCreation>("create-metadata")
+                .WithDescription("Creates registry metadata for the given components.")
+                .WithExample(
+                    "registry",
+                    "create-metadata",
+                    "Components/*",
+                    "-o",
+                    "dist/components",
+                    "--registry-uri",
+                    "https://raw.github.com/repo"
+                );
+        }
+    );
 });
-
 
 return app.Run(args);
